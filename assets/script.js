04650b9b4a3d6ae101ed6999843ab80127888590
devinshade <devindanielle93@gmail.com>
2024-06-09 20:53:57 -0600

    bootstrap footer styling

--- assets/script.js
+++ assets/script.js
@@ -2,3 +2,5 @@
 console.log("JavaScript file is connected properly!");
 // alert("JavaScript file is connected properly!");
 
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
