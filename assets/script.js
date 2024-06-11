40de8f35c6235ff881e15d1c263d3d93aed67266
devinshade <devindanielle93@gmail.com>
2024-06-10 19:59:22 -0600

    bootstrap/carousel bootcamp projects and accordion skills

--- assets/script.js
+++ assets/script.js
@@ -2,5 +2,4 @@
 console.log("JavaScript file is connected properly!");
 // alert("JavaScript file is connected properly!");
 
-const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
-const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+import * as Popper from "@popperjs/core"
